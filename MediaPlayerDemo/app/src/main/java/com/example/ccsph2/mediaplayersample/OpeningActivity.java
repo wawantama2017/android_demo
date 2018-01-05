f75b9a0597fa4871dd641a5fdb8bd3b47fc68b92
wawantama2017 <wawankurjp@gmail.com>
2018-01-05 20:24:46 +0900

    Add opening activity

---
+++ MediaPlayerDemo/app/src/main/java/com/example/ccsph2/mediaplayersample/OpeningActivity.java
@@ -0,0 +1,31 @@
+package com.example.ccsph2.mediaplayersample;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import com.example.ccsph2.mediaplayerdemo.R;
+
+public class OpeningActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_opening);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+}
