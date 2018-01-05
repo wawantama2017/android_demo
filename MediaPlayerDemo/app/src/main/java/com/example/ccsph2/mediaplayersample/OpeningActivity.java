0e70e1cd59604247054207a5a7b3b4e51b239a26
wawantama2017 <wawankurjp@gmail.com>
2018-01-05 20:50:00 +0900

    Reorder Activity

--- MediaPlayerDemo/app/src/main/java/com/example/ccsph2/mediaplayersample/OpeningActivity.java
+++ MediaPlayerDemo/app/src/main/java/com/example/ccsph2/mediaplayersample/OpeningActivity.java
@@ -1,5 +1,6 @@
 package com.example.ccsph2.mediaplayersample;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -7,6 +8,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
+import com.example.ccsph2.mediaplayerdemo.MainActivity;
 import com.example.ccsph2.mediaplayerdemo.R;
 
 public class OpeningActivity extends AppCompatActivity {
@@ -22,10 +24,14 @@ public class OpeningActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                //Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                //        .setAction("Action", null).show();
+
+                // Send Intent to start main activity
+                //Intent intent = new Intent(this, MainActivity.class);
+                //intent.putExtra("MY_HAND",view.getId());
+                //startActivity(intent);
             }
         });
     }
-
 }
